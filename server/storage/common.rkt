e9e87b0af9312ace4d69d74ad37922984e972b24
jcollard <josephmcollard@gmail.com>
2014-06-04 13:51:22 -0400

    Abstracted common bits out from local / s3.

---
+++ server/storage/common.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+
+
+ ;; Helper functions
+(provide intercalate)
+(define (intercalate v ls)
+  (if (null? ls) ls
+      (cdr 
+       (foldr 
+        (lambda (x xs) 
+          (cons v (cons x xs))) 
+        '() ls))))
+
+(provide root)
+(define root '("home" "admiraledu" "files"))
+
+(define path-delim "/")
+
+(provide to-path)
+(define (to-path ls)
+  (foldr string-append "" (cons path-delim (intercalate path-delim ls))))
+
+(define (create-path-list class user assignment step version)
+  (append root (list class user assignment step version)))
+
+(provide create-directory)
+(define (create-directory class user assignment step version)
+  (letrec ((path-list (list class user assignment step version))
+           (helper (lambda (acc rest)
+                          ;; This is O(n^2) where n is the number of sub-directories. 
+                          ;; Could be improved to O(n). However, n is a constant so in practice
+                          ;; this shouldn't be an issue.
+                     (let ((path (to-path (reverse acc))))
+                       (if (not (directory-exists? path)) 
+                           (make-directory path) '())
+                       (if (null? rest) path (helper (cons (car rest) acc) (cdr rest)))))))
+    (helper (reverse root) path-list)))
+
+
+;; TODO: Eventually we want to detect the archive type and choose the correct program to extract it.
+;; This also needs to be able to handle invalid file types
+(provide unarchive)
+(define (unarchive path)
+  (system (string-append "tar -xf " path "/submission.tar -C " path)))
--- server/storage/s3.rkt
+++ server/storage/s3.rkt
@@ -1,43 +1,12 @@
 #lang racket
 
 (require (planet gh/aws:1:5))
-(require "../database/mysql.rkt")
+(require "../database/mysql.rkt"
+         "common.rkt")
 
 (read-keys "/home/admiraledu/aws-credentials")
 (ensure-have-keys)
 (define bucket-name "cmpsci220-f14")
-
-(define path-delim "/")
-
-(define (to-path ls)
-  (foldr string-append "" (cons path-delim (intercalate path-delim ls))))
-
- ;; Helper functions
-(define (intercalate v ls)
-  (if (null? ls) ls
-      (cdr 
-       (foldr 
-        (lambda (x xs) 
-          (cons v (cons x xs))) 
-        '() ls))))
-
-(define root '("home" "admiraledu" "files"))
-
-(define (create-path-list class user assignment step version)
-  (append root (list class user assignment step version)))
-
-(provide create-directory)
-(define (create-directory class user assignment step version)
-  (letrec ((path-list (list class user assignment step version))
-           (helper (lambda (acc rest)
-                          ;; This is O(n^2) where n is the number of sub-directories. 
-                          ;; Could be improved to O(n). However, n is a constant so in practice
-                          ;; this shouldn't be an issue.
-                     (let ((path (to-path (reverse acc))))
-                       (if (not (directory-exists? path)) 
-                           (make-directory path) '())
-                       (if (null? rest) path (helper (cons (car rest) acc) (cdr rest)))))))
-    (helper (reverse root) path-list)))
 
 (provide upload-submission)
 (define (upload-submission class user assignment step data)
@@ -52,12 +21,18 @@
     (submission:create assignment class step user)
     (delete-directory/files path)))
 
+;; Deletes all files in the bucket
+(define (clear-bucket)
+  (let* ((files (ls (string-append bucket-name "/")))
+         (paths (lambda (name) (string-append bucket-name "/" name)))
+         (bucket+paths (map paths files))
+         (remove (lambda (path) (delete path))))
+    (map remove bucket+paths))) 
+
+
 (define (push-contents path)
   (let ((files (find-files (lambda (file) (not (directory-exists? file))) path))
         (put (lambda (path) 
                (put/file (string-append bucket-name (path->string path)) path))))
     (map put files)))
     
-
-(define (unarchive path)
-  (system (string-append "tar -xf " path "/submission.tar -C " path)))

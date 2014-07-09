fd9d3e2699f8ce77f6f46f855f2ee4e3c94c2e73
jcollard <josephmcollard@gmail.com>
2014-07-09 14:45:26 -0400

    Added basic authoring structs.

---
+++ server/authoring/assignment.rkt
@@ -0,0 +1,158 @@
+#lang racket
+
+(struct Assignment (name id description steps) #:transparent)
+
+(define (assignment name id description . steps)
+  (Assignment name id description steps))
+
+(struct Step (id instructions reviews) #:transparent)
+
+(define (step id instructions . reviews)
+  (Step id instructions reviews))
+
+(struct Student-submission (rubric) #:transparent)
+
+(define (student-submission . elements)
+  (Student-submission (Rubric elements)))
+
+(struct Instructor-solution (id rubric) #:transparent)
+
+(define (instructor-solution id . elements)
+  (Instructor-solution id (Rubric elements)))
+
+(struct Rubric (elements) #:transparent)
+
+(define (rubric . elements)
+  (Rubric elements))
+
+(struct instruction (text) #:transparent)
+
+(struct likert (id text min-label max-label granularity) #:transparent)
+
+(struct free-form (id text) #:transparent)
+
+(define test-assignment
+  (assignment "Clocks"
+              "clocks"
+              "Students develop functions representing an alarm clock."
+              
+               (step "tests"
+                     "Submit your test cases. Do not submit any clock implementation."
+                     (instructor-solution "Poor Tests"
+                                          
+                                          (likert "correctness"
+                                                  "These tests are correct."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "Provide feedback on tests that are not correct by clicking on the line number and adding a comment.")
+                                          
+                                          (likert "coverage"
+                                                  "These tests cover the possible inputs."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (free-form "not-covered"
+                                                     "If applicable, provide inputs that are not covered by the tests."))
+                     
+                     (instructor-solution "Good Tests"
+                                          
+                                          (likert "correctness"
+                                                  "These tests are correct."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "Provide feedback on tests that are not correct by clicking on the line number and adding a comment.")
+                                          
+                                          (likert "coverage"
+                                                  "These tests cover the possible inputs."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (free-form "not-covered"
+                                                     "If applicable, provide inputs that are not covered by the tests."))
+                     
+                     (student-submission (likert "correctness"
+                                                  "These tests are correct."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "Provide feedback on tests that are not correct by clicking on the line number and adding a comment.")
+                                          
+                                          (likert "coverage"
+                                                  "These tests cover the possible inputs."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (free-form "not-covered"
+                                                     "If applicable, provide inputs that are not covered by the tests.")))
+               
+               (step "implementation"
+                     "Submit all of your test cases and your clock implementation."
+                     
+                     (instructor-solution "Poor Implementation"
+                                          
+                                          (likert "behavior"
+                                                  "This code correctly implements the desired behavior."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "If applicable, leave inline feedback where the incorrect behaviors exist.")
+                                          
+                                          (likert "structure"
+                                                  "This code is structured well."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "If applicable, leave inline feedback where the code is not structured well.")
+                                          
+                                          (free-form "feedback"
+                                                     "Additional Comments"))
+                     
+                     (instructor-solution "Good Implementation"
+                                          
+                                          (likert "behavior"
+                                                  "This code correctly implements the desired behavior."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "If applicable, leave inline feedback where the incorrect behaviors exist.")
+                                          
+                                          (likert "structure"
+                                                  "This code is structured well."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                          (instruction "If applicable, leave inline feedback where the code is not structured well.")
+                                          
+                                          (free-form "feedback"
+                                                     "Additional Comments"))
+                     
+                     (student-submission (likert "behavior"
+                                                  "This code correctly implements the desired behavior."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                         (instruction "If applicable, leave inline feedback where the incorrect behaviors exist.")
+                                          
+                                         (likert "structure"
+                                                  "This code is structured well."
+                                                  "Disagree"
+                                                  "Agree"
+                                                  9)
+                                          
+                                         (instruction "If applicable, leave inline feedback where the code is not structured well.")
+                                          
+                                         (free-form "feedback"
+                                                     "Additional Comments")))))

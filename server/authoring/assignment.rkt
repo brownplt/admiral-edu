3be115302e9fb390bf16b56146f666e94614b454
jcollard <josephmcollard@gmail.com>
2014-07-11 10:53:07 -0400

    Finished ->yaml and yaml-> functions. sample.yaml now parses into assignment structs.

--- server/authoring/assignment.rkt
+++ server/authoring/assignment.rkt
@@ -10,14 +10,94 @@
 (define (assignment name id description . steps)
   (Assignment name id description steps))
 
+(define (hash-has-keys? hash . els)
+  (ands (map (lambda (x) (hash-has-key? hash x)) els)))
+
+(define (yaml->assignment yaml) 
+  (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->assignment "yaml" yaml)]
+        [(not (= 4 (hash-count yaml))) (raise-user-error "Expected record with 4 fields: `name`, `id`, `description`, and `steps`")]
+        [(not (hash-has-keys? yaml "name" "id" "description" "steps")) (raise-user-error "Expected record with 4 fields: `name`, `id`, `description`, and `steps`")]
+        [else (let ((id (hash-ref yaml "id"))
+                    (name (hash-ref yaml "name"))
+                    (description (hash-ref yaml "description"))
+                    (steps (map yaml->step (hash-ref yaml "steps"))))
+                (Assignment name id description steps))]))
+
+(define (assignment->yaml assignment)
+  (cond [(not (Assignment? assignment)) (raise-argument-error 'assignment->yaml "Assignment" assignment)]
+        [else (let ((name (Assignment-name assignment))
+                    (id (Assignment-id assignment))
+                    (description (Assignment-description assignment))
+                    (steps (map step->yaml (Assignment-steps assignment))))
+                `#hash(("name" . ,name) ("id" . ,id) ("description" . ,description) ("steps" . ,steps)))]))
+                
+
 (struct Step (id instructions reviews) #:transparent)
 
 (define (step id instructions . reviews)
   (Step id instructions reviews))
 
+(define (step->yaml step)
+  (cond [(not (Step? step)) (raise-argument-error 'step->yaml "Step" step)]
+        [else (let* ((id (Step-id step))
+                     (instructions (Step-instructions step))
+                     (reviews (map review->yaml (Step-reviews step)))
+                     (partial `(("id" . ,id) ("instructions" . ,instructions))))
+                (make-hash (if (null? reviews) partial (cons `("reviews" . ,reviews) partial))))]))
+
+(define (yaml->step yaml)
+  (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->step "yaml" yaml)]
+        [(not (or (= 2 (hash-count yaml)) (= 3 (hash-count yaml)))) (raise-user-error "Expected two or three fields: `id`, `instructions`, and optionally `reviews`." yaml)]
+        [(not (hash-has-key? yaml "id")) (raise-user-error "Expected two or three fields: `id`, `instructions`, and optionally `reviews`." yaml)]
+        [(not (hash-has-key? yaml "instructions")) (raise-user-error "Expected two or three fields: `id`, `instructions`, and optionally `reviews`." yaml)]
+        [else (let ((id (hash-ref yaml "id"))
+                    (instructions (hash-ref yaml "instructions")))
+                (cond [(= 2 (hash-count yaml)) (step id instructions)]
+                      [(not (hash-has-key? yaml "reviews")) (raise-user-error "Expected two or three fields: `id`, `instructions`, and optionally `reviews`." yaml)]
+                      [else (let ((reviews (hash-ref yaml "reviews")))
+                              (Step id instructions (map yaml->review reviews)))]))]))        
+
+
 (struct student-submission (amount rubric) #:transparent)
 
+(define (student-submission->yaml submission) 
+  (cond [(not (student-submission? submission)) (raise-argument-error 'student-submission->yaml "student-submission" submission)]
+        [else (let ((amount (student-submission-amount submission))
+                    (rubric (rubric->yaml (student-submission-rubric submission))))
+                `#hash(("student-submission" . #hash(("amount" . ,amount) ("rubric" . ,rubric)))))]))
+
+(define (yaml->student-submission yaml) 
+  (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->student-submission "yaml" yaml)]
+        [(not (= 1 (hash-count yaml))) (raise-user-error "Expected a single record `student-submission`." yaml)]
+        [(not (hash-has-key? yaml "student-submission")) (raise-user-error "Expected a single record `student-submission`." yaml)]
+        [else (let ((rec (hash-ref yaml "student-submission")))
+                (cond [(not (= 2 (hash-count rec))) (raise-user-error "Expected two fields `amount` and `rubric`." rec)]
+                      [(not (hash-has-key? rec "amount")) (raise-user-error "Expected two fields `amount` and `rubric`." rec)]
+                      [(not (hash-has-key? rec "rubric")) (raise-user-error "Expected two fields `amount` and `rubric`." rec)]
+                      [else (let ((amount (hash-ref rec "amount"))
+                                  (rubric (hash-ref rec "rubric")))
+                              (student-submission amount (yaml->rubric rubric)))]))]))
+
+
 (struct instructor-solution (id rubric) #:transparent)
+
+(define (instructor-solution->yaml solution)
+  (cond [(not (instructor-solution? solution)) (raise-argument-error 'instructor-solution->yaml "instructor-solution" solution)]
+        [else (let ((id (instructor-solution-id solution))
+                    (rubric (rubric->yaml (instructor-solution-rubric solution))))
+                `#hash(("instructor-solution" . #hash(("id" . ,id) ("rubric" . ,rubric)))))]))
+
+(define (yaml->instructor-solution yaml)
+  (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->instructor-solution "yaml" yaml)]
+        [(not (= 1 (hash-count yaml))) (raise-user-error "Expected a single record `instructor-solution`." yaml)]
+        [(not (hash-has-key? yaml "instructor-solution")) (raise-user-error "Expected a single record `instructor-solution`." yaml)]
+        [else (let ((rec (hash-ref yaml "instructor-solution")))
+                (cond [(not (= 2 (hash-count rec))) (raise-user-error "Expected two fields `id` and `rubric`." rec)]
+                      [(not (hash-has-key? rec "id")) (raise-user-error "Expected two fields `id` and `rubric`." rec)]
+                      [(not (hash-has-key? rec "rubric")) (raise-user-error "Expected two fields `id` and `rubric`." rec)]
+                      [else (let ((id (hash-ref rec "id"))
+                                  (rubric (hash-ref rec "rubric")))
+                              (instructor-solution id (yaml->rubric rubric)))]))]))
 
 (provide (contract-out 
           [struct Rubric ((elements (non-empty-listof rubric-element?)))]))
@@ -29,13 +109,11 @@
 (define (rubric->yaml rubric)
   (cond [(not (Rubric? rubric)) (raise-argument-error 'rubric->yaml "Rubric" rubric)]
         [else (let ((elements (Rubric-elements rubric)))
-                `#hash(("rubric" . ,(map rubric-element->yaml elements))))]))
+                (map rubric-element->yaml elements))]))
 
 (define (yaml->rubric yaml)
   (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->rubric "yaml" yaml)]
-        [(not (= 1 (hash-count yaml))) (raise-user-error "Expected a single record `rubric`." yaml)]
-        [(not (hash-has-key? yaml "rubric")) (raise-user-error "Expected a single record `rubric`." yaml)]
-        [else (let ((elems (map yaml->element (hash-ref yaml "rubric"))))
+        [else (let ((elems (map yaml->element yaml)))
                 (Rubric elems))]))
 
 (define (yaml->element yaml)
@@ -59,7 +137,18 @@
 (define (instruction->yaml instruction)
   (cond [(not (instruction? instruction)) (raise-argument-error 'instruction->yaml "instruction" instruction)]
         [else (let ((text (instruction-text instruction)))
-                `#hash(("instruction" . #hash(("text" . ,text)))))]))
+                `#hash(("instruction" . ,text)))]))
+
+
+(define (yaml->instruction yaml)
+  (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->instruction "yaml" yaml)]
+        [(not (= 1 (hash-count yaml))) (raise-user-error "Expected a single record `instruction`.")]
+        [(not (hash-has-key? yaml "instruction")) (raise-user-error "Expected a single record `instruction`.")]
+        [else (let ((text (hash-ref yaml "instruction")))
+                (instruction text))]))
+
+(let ((i (instruction "Test instructions")))
+  (check-equal? i (yaml->instruction (instruction->yaml i))))
 
 (struct likert (id text min max granularity) #:transparent)
 
@@ -109,18 +198,14 @@
 (let ((l (likert "test-id" "Test instructions" "Disagree" "Agree" 9)))
   (check-equal? l (yaml->likert (likert->yaml l))))
 
-(define yaml->instruction
-  (yaml->rubric-element "instruction" instruction "text"))
-
-(let ((i (instruction "Test instructions")))
-  (check-equal? i (yaml->instruction (instruction->yaml i))))
-
 (define (ors els) 
   (cond
     [(not ((listof boolean?) els)) (raise-argument-error 'ors "non-empty-listof boolean?" els)]
     [else (match els
             ['() #f]
             [(cons head tail) (if head #t (ors tail))])]))
+
+(define (ands els) (foldr (lambda (x y) (and x y)) #t els))
 
 (define (one-of? cs)
   (cond
@@ -132,6 +217,21 @@
                                  [(cons head? tail) (if (head? el) #t (helper tail))]))))
               (helper cs)))]))
 
+(define review?
+  (one-of? (list student-submission? instructor-solution?)))
+
+
+(define (review->yaml review)
+  (cond [(not (review? review)) (raise-argument-error 'review->yaml "review" review)]
+        [(instructor-solution? review) (instructor-solution->yaml review)]
+        [(student-submission? review) (student-submission->yaml review)]))
+
+(define (yaml->review yaml)
+  (cond [(not (yaml? yaml)) (raise-argument-error 'yaml->review "yaml" yaml)]
+        [(not (= 1 (hash-count yaml))) (raise-user-error "Expected one field either `instructor-solution` or `student-submission`." yaml)]
+        [(hash-has-key? yaml "instructor-solution") (yaml->instructor-solution yaml)]
+        [(hash-has-key? yaml "student-submission") (yaml->student-submission yaml)]
+        [else (raise-user-error "Expected one field either `instructor-solution` or `student-submission`." yaml)]))
 
 (define rubric-element? 
   (one-of? 
@@ -275,21 +375,49 @@
 
 (define test-rubric
   (rubric
-                                         (likert "behavior"
-                                                  "This code correctly implements the desired behavior."
-                                                  "Disagree"
-                                                  "Agree"
-                                                  9)
-                                          
-                                         (instruction "If applicable, leave inline feedback where the incorrect behaviors exist.")
-                                          
-                                         (likert "structure"
-                                                  "This code is structured well."
-                                                  "Disagree"
-                                                  "Agree"
-                                                  9)
-                                          
-                                         (instruction "If applicable, leave inline feedback where the code is not structured well.")
-                                          
-                                         (free-form "feedback"
-                                                     "Additional Comments")))
+   (likert "behavior"
+           "This code correctly implements the desired behavior."
+           "Disagree"
+           "Agree"
+           9)
+   
+   (instruction "If applicable, leave inline feedback where the incorrect behaviors exist.")
+   
+   (likert "structure"
+           "This code is structured well."
+           "Disagree"
+           "Agree"
+           9)
+   
+   (instruction "If applicable, leave inline feedback where the code is not structured well.")
+   
+   (free-form "feedback"
+              "Additional Comments")))
+
+(check-equal? test-rubric (yaml->rubric (rubric->yaml test-rubric)))
+
+(let ((submission (instructor-solution "test-id" test-rubric)))
+  (check-equal? submission (yaml->instructor-solution (instructor-solution->yaml submission)))
+  (check-equal? submission (yaml->review (review->yaml submission))))
+
+(check-true (yaml? (student-submission->yaml (student-submission 1 test-rubric))))
+
+(let ((submission (student-submission 2 test-rubric)))
+  (check-equal? submission (yaml->student-submission (student-submission->yaml submission)))
+  (check-equal? submission (yaml->review (review->yaml submission))))
+
+(check-true (yaml? (step->yaml (step "test-id" "test instructions"))))
+(check-true (yaml? (step->yaml (step "test-id" "test instructions" (instructor-solution "test-id" test-rubric)))))
+(check-true (yaml? (step->yaml (step "test-id" "test instructions" (instructor-solution "test-id" test-rubric) (student-submission 2 test-rubric)))))
+
+(let ((step (step "test-id" "test instructions")))
+  (check-equal? step (yaml->step (step->yaml step))))
+
+(let ((step (step "test-id" "test instructions" (instructor-solution "test-id" test-rubric))))
+  (check-equal? step (yaml->step (step->yaml step))))
+
+(let ((step (step "test-id" "test instructions" (instructor-solution "test-id" test-rubric) (student-submission 2 test-rubric))))
+  (check-equal? step (yaml->step (step->yaml step))))
+
+(check-true (yaml? (assignment->yaml test-assignment)))
+(check-equal? test-assignment (yaml->assignment (assignment->yaml test-assignment)))

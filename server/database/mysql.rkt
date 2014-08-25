65cc2d8683004fb251a7916abdd4cae8be6519a8
jcollard <josephmcollard@gmail.com>
2014-08-25 14:10:45 -0400

    Removed redundant provides.

--- server/database/mysql.rkt
+++ server/database/mysql.rkt
@@ -37,56 +37,22 @@
 
 
 ;; User Table
-(provide user:all user:create user:exists?)
+(provide (all-from-out "mysql/user.rkt"))
 
 ;; Class Table
-(provide class:all class:create class:exists?)
+(provide (all-from-out "mysql/class.rkt"))
 
 ;; Role Table
-(provide role:select role:associate role:in-class role:user-uid role:user-class role:user-role role:exists?)
+(provide (all-from-out "mysql/role.rkt"))
 
 ;; Roles Table
-(provide roles:create roles:get-role roles:role-id roles:role-name roles:role-can-edit roles:all)
+(provide (all-from-out "mysql/roles.rkt"))
 
 ;; Assignment Table
-(provide assignment:exists? assignment:create assignment:all assignment:list
-         assignment:open assignment:close assignment:mark-ready assignment:mark-not-ready
-         assignment:record-class assignment:record-id assignment:record-open assignment:record-ready
-         assignment:record?)
+(provide (all-from-out "mysql/assignment.rkt"))
 
 ;; Submission Table
-(provide submission:table 
-         submission:record submission:record?
-         submission:assignment-id submission:assignment-id-type submission:record-assignment
-         submission:class-id submission:class-id-type submission:record-class
-         submission:step-id submission:step-id-type submission:record-step
-         submission:user-id submission:user-id-type submission:record-user
-         submission:time-stamp submission:time-stamp-type submission:record-time-stamp
-         submission:create submission:list submission:count
-         submission:exists?
-         submission:create-instructor-solution
-         submission:times-reviewed)
-
+(provide (all-from-out "mysql/submission.rkt"))
 
 ;; Review Table
-(provide review:table
-         review:create
-         review:record review:record? review:record-class-id 
-         review:record-assignment-id review:record-step-id review:record-reviewee-id 
-         review:record-reviewer-id review:record-completed review:record-review-id
-         review:record-hash
-         review:assignment-id review:assignment-id-type
-         review:step-id review:step-id-type
-         review:class-id review:class-id-type
-         review:reviewee-id review:reviewee-id-type
-         review:reviewer-id review:reviewer-id-type
-         review:time-stamp review:time-stamp-type
-         review:completed? review:count-completed
-         review:select-by-hash review:select-reviews
-         review:assign-student-reviews
-         review:assign-instructor-solution
-         review:select-reviews
-         review:select-assigned-reviews
-         review:mark-complete
-         review:hash
-         review:select-feedback)
+(provide (all-from-out "mysql/review.rkt"))

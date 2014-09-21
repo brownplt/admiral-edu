76861118c4f9aef904acbd41b6fdf202d9095545
jcollard <josephmcollard@gmail.com>
2014-09-20 20:14:56 -0400

    Added types to main database functions.

--- server/database/mysql.rkt
+++ server/database/mysql.rkt
@@ -1,7 +1,6 @@
-#lang racket
+#lang typed/racket
 
-(require "mysql/common.rkt"
-         db
+(require "mysql/typed-db.rkt"
          (prefix-in class: "mysql/class.rkt") 
          (prefix-in user: "mysql/user.rkt")
          (prefix-in role: "mysql/role.rkt")
@@ -11,34 +10,6 @@
          (prefix-in review: "mysql/review.rkt")
          (prefix-in system: "mysql/system.rkt")
          (prefix-in migrate: "mysql/migrate.rkt"))
-
-;; Initializes the database.
-(provide init-db)
-(define (init-db)
-  (user:init)
-  (class:init)
-  (role:init)
-  (roles:init)
-  (assignment:init)
-  (submission:init)
-  (review:init)
-  (system:init))
-
-;; Returns #t if init-db has been called and #f otherwise
-(provide init-db?)
-(define (init-db?)
-  (let* ((conn (make-sql-conn))
-         (query (merge "SELECT COUNT(*)"
-                       "FROM information_schema.tables "
-                       "WHERE table_schema = 'captain_teach'"
-                       "AND table_name = ?;"))
-         (prep (prepare conn query))
-         (result (query-row conn prep review:table))
-         (count (vector-ref result 0)))
-    (release conn)
-    (> count 0)))
-
-
 
 ;; User Table
 (provide (all-from-out "mysql/user.rkt"))
@@ -67,8 +38,34 @@
 ;; Migration functions
 (provide (all-from-out "mysql/migrate.rkt"))
 
+
+;; Initializes the database.
+(provide init-db)
+(: init-db (-> Void))
+(define (init-db)
+  (user:init)
+  (class:init)
+  (role:init)
+  (roles:init)
+  (assignment:init)
+  (submission:init)
+  (review:init)
+  (system:init))
+
+;; Returns #t if init-db has been called and #f otherwise
+(provide init-db?)
+(: init-db? (-> Boolean))
+(define (init-db?)
+  (let* ((query (merge "SELECT COUNT(*)"
+                       "FROM information_schema.tables "
+                       "WHERE table_schema = 'captain_teach'"
+                       "AND table_name = ?;"))
+         (result (query-value query review:table)))
+    (> (cast result Exact-Nonnegative-Integer) 0)))
+
 ;; Permanently removes all references to an assignment from the database
 (provide database:delete-assignment)
+(: database:delete-assignment (String String -> Void))
 (define (database:delete-assignment class-id assignment-id)
   (review:delete-assignment class-id assignment-id)
   (assignment:delete-assignment class-id assignment-id)

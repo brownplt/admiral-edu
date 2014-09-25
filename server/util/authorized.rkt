20d2955f44dd95a27f44f72be30b43ce9269e925
jcollard <josephmcollard@gmail.com>
2014-09-25 15:38:07 -0400

    Had to remove types which were causing contract errors in untyped code.

--- server/util/authorized.rkt
+++ server/util/authorized.rkt
@@ -1,13 +1,13 @@
-#lang typed/racket
+#lang racket
 
 (require "../base.rkt"
          "../pages/typed-xml.rkt")
 
-(require/typed (prefix-in error: "../pages/errors.rkt")
-               [error:not-authorized (-> Any)])
+(require (prefix-in error: "../pages/errors.rkt"))
+               ;[error:not-authorized (-> Any)])
 
 
-(: role (ct-session -> roles:Record))
+;(: role (ct-session -> roles:Record))
 (define (role session)
   (let* ((class (ct-session-class session))
          (uid (ct-session-uid session))
@@ -18,14 +18,14 @@
 ; TODO: Eventually this should have the following type
 ;(: can-edit (All (a) (-> ct-session (-> a * (Listof (U XExpr Void))) a * (Listof (U XExpr Void)))))
 (provide can-edit)
-(: can-edit (All (a b) (-> ct-session (-> a * b) a * Any)))
+;(: can-edit (All (a b) (-> ct-session (-> a * b) a * Any)))
 (define (can-edit session f . args)
   (cond [(can-edit? session) (apply f args)]
         [else (error:not-authorized)]))
 
 
 (provide can-edit?)
-(: can-edit? (ct-session -> Boolean))
+;(: can-edit? (ct-session -> Boolean))
 (define (can-edit? session)
   (let ((session-role (role session)))
     (roles:Record-can-edit session-role)))

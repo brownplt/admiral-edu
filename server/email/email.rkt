fb6fae866438799338535c859957ccaf33a0ea73
jcollard <josephmcollard@gmail.com>
2014-09-27 08:24:06 -0400

    Added types to email.

--- server/email/email.rkt
+++ server/email/email.rkt
@@ -1,32 +1,59 @@
-#lang racket
+#lang typed/racket
 
-(require net/smtp
-         net/head
-         openssl)
+(require/typed net/smtp
+               [smtp-send-message (->* (String String (Listof String) String String)
+                                       (#:port-no Integer
+                                        #:auth-user (U String #f)
+                                        #:auth-passwd (U String #f)
+                                        #:tls-encode EncodeFunc)
+                                       Void)])
 
-(require "../configuration.rkt"
-         "../util/config-file-reader.rkt")
+;                                      [#:auth-user (U String #f)]
+;                                      [#:auth-passwd (U String #f)]
+;                                      [#:tls-encode Any]
+;                                      [Exact-Nonnegative-Integer]
+;                                       Void)])
 
+(require/typed net/head
+               [standard-message-header (String (Listof String) (Listof String) (Listof String) String -> String)])
+
+(define-type EncodeFunc (->* (Input-Port Output-Port)
+                             (#:mode Symbol
+                              #:context Any
+                              #:encrypt Any
+                              #:close-original? Boolean
+                              #:shutdown-on-close? Boolean
+                              #:error/ssl Procedure
+                              #:hostname (U String #f))
+                             (Values Input-Port Output-Port)))
+
+(require/typed openssl
+               [ports->ssl-ports EncodeFunc])
+
+(require "../configuration.rkt")
+
+(: lines (String -> (Listof String)))
 (define (lines string)
   (string-split string "\n"))
 
 (provide send-email)
+(: send-email (String String String -> Boolean))
 (define (send-email uid subject message)
-  (let* ((from (string-append "tech@" server-name))
+  (let* ((from (string-append "tech@" (assert server-name string?)))
          (to (list uid))
          (header (standard-message-header from to '() '() subject))
          (split-message (lines message)))
     (cond [(email-okay uid) (begin
                               (smtp-send-message 
-                               mail-server 
+                               (assert mail-server string?)
                                from 
                                to 
                                header 
                                split-message
-                               #:auth-user mail-username
-                               #:auth-passwd mail-password
+                               #:auth-user (assert mail-username string?)
+                               #:auth-passwd (assert mail-password string?)
                                #:tls-encode ports->ssl-ports
-                               mail-port)
+                               #:port-no (assert mail-port integer?))
                               #t)]
           [else #f])))
 
@@ -38,6 +65,7 @@
 ;; Blacklist @student.edu email addresses and default-submission addresses
 (define black-list-email-regexp "([.]*@student.edu|default-submission[.]*)")
 
+(: email-okay (String -> Boolean))
 (define (email-okay uid)
   (let ((black-listed (regexp-match black-list-email-regexp uid))
         (okay (regexp-match-exact? okay-email-regexp uid)))

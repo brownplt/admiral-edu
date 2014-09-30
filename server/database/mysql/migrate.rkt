71ec6072743cca83f031f04ed798b8a47984a339
jcollard <josephmcollard@gmail.com>
2014-09-30 14:45:58 -0400

    Added types to migrate.

--- server/database/mysql/migrate.rkt
+++ server/database/mysql/migrate.rkt
@@ -1,13 +1,17 @@
-#lang racket
+#lang typed/racket
 
 (require "../../util/basic-types.rkt"
-         (prefix-in system: "system.rkt")
-         (prefix-in v1: "migrate-0-1.rkt"))
+         (prefix-in system: "system.rkt"))
+
+(require/typed (prefix-in v1: "migrate-0-1.rkt")
+               [v1:check-migrated (-> (Result Void))]
+               [v1:migrate (-> (Result Void))])
 
 
 ;; ( -> Result void?)
 ;; Success if at the current 
 (provide check-migrated)
+(: check-migrated (-> (Result Void)))
 (define (check-migrated)
   (when (Failure? (v1:check-migrated)) (v1:migrate))
   (let ((version (system:select-version)))

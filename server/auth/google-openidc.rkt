e86e8c628e421f14a9516f7bae2db1628c0d4c21
jcollard <josephmcollard@gmail.com>
2014-09-30 14:51:14 -0400

    Added types to google auth.

--- server/auth/google-openidc.rkt
+++ server/auth/google-openidc.rkt
@@ -1,8 +1,12 @@
-#lang racket
+#lang typed/racket
 
-(require web-server/http/bindings)
+(require/typed web-server/http/bindings
+               [extract-binding/single (Symbol (Listof (Pairof Symbol (U String Bytes))) -> (U String Bytes))]
+               [exists-binding? (Symbol (Listof (Pairof Symbol (U String Bytes))) -> Boolean)]
+               [request-headers (Any -> (Listof (Pairof Symbol (U String Bytes))))])
   
 (provide headers->uid)  
+(: headers->uid ((Listof (Pairof Symbol (U String Bytes))) -> (U String Bytes 'invalid-session)))
 (define (headers->uid headers)
   (if (exists-binding? 'oidc_claim_email headers)
       (extract-binding/single 'oidc_claim_email headers)

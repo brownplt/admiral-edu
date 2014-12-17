77323f0b663402f7f7aee455dfa09537002ecb87
jcollard <josephmcollard@gmail.com>
2014-12-17 09:55:09 -0500

    Added # comments in configuration file.

--- server/util/config-file-reader.rkt
+++ server/util/config-file-reader.rkt
@@ -7,8 +7,15 @@
 (: read-conf (->* (Path-String) (String) (HashTable String String)))
 (define (read-conf file-path [delim "="])
   (let*: ([contents : String  (file->string file-path)]
-          [lines : (Listof String) (string-split contents "\n")]
+          [lines : (Listof String) (filter (compose not comment?) (string-split contents "\n"))]
           [splits : (Listof (Listof String)) (map (lambda: ([s : String]) (string-split s delim)) lines)]
           [len-2 : (Listof (Listof String)) (filter (lambda: ([s : (Listof String)]) (> (length s) 1)) splits)]
           [result : (Listof (Pairof String String)) (map (lambda: ([x : (Listof String)]) (cons (car x) (string-join (cdr x) delim))) len-2)])
     (make-hash result)))
+
+(: comment? (String -> Boolean))
+(define (comment? line)
+  (let ((trimmed (string-trim line)))
+    (cond [(= (string-length trimmed) 0) #f]
+          [(eq? (string-ref trimmed 0) #\#) #t]
+          [else #f])))

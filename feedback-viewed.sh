bfdb2a3f31d60207a7e7bfa0e908b962625f8689
Joseph Collard <jcollard@users.noreply.github.com>
2015-08-27 05:41:27 -0400

    Update feedback-viewed.sh

--- feedback-viewed.sh
+++ feedback-viewed.sh
@@ -1,8 +1,9 @@
 #/bin/bash
-file="$(pwd)/feedback-viewed-$1.csv"
+file="$(pwd)/feedback-viewed-$1-$2.csv"
 cmd="SELECT reviewee_id, time_stamp, feedback_viewed_time_stamp"
 cmd="$cmd FROM review"
 cmd="$cmd WHERE assignment_id='$1' "
+cmd="$cmd AND step_id='$2' "
 cmd="$cmd AND reviewee_id NOT LIKE 'default%'  "
 cmd="$cmd ORDER BY feedback_viewed_time_stamp ASC "
 cmd="$cmd INTO OUTFILE '$file'"

0ff22d990d176db97ab59c3d1e7fd1e81a5a9986
John Clements <clements@racket-lang.org>
2016-06-24 17:35:15 -0700

    more info from failing procs in storage procs

--- server/storage/storage-basic.rkt
+++ server/storage/storage-basic.rkt
@@ -9,83 +9,71 @@
 (provide storage-init)
 
 ;; signal an error, must call (storage-init) first...
-(define (storage-not-initialized . any)
-  (error 'storage-mode
+(define ((storage-not-initialized proc) . any)
+  (error proc
          "must call (storage-init) before using storage functions"))
 
+
+(define-syntax fun-maker
+  (syntax-rules ()
+    [(_ name)
+     (begin (provide name)
+            (define name (storage-not-initialized (quote name))))]))
 
 ; (path -> string)
 ; Given the path to a file, returns the contents of the retrieved file
 ; Otherwise retures Failure with a message.
-(provide retrieve-file)
-(define retrieve-file storage-not-initialized)
-(define (set-retrieve-file proc) (set! retrieve-file proc))
-
-(provide retrieve-file-bytes)
-(define retrieve-file-bytes storage-not-initialized)
-(define (set-retrieve-file-bytes proc) (set! retrieve-file-bytes proc))
+(fun-maker retrieve-file)
+(fun-maker retrieve-file-bytes)
 
 ; (path -> string -> ())
 ; Given a path and the contents to a file, writes that file (over writing any existing file).
-(provide write-file)
-(define write-file storage-not-initialized)
-(define (set-write-file proc) (set! write-file proc))
+(fun-maker write-file)
 
 ; (path -> ())
 ; Deletes the specified path. If it is a file, removes the specified file. If it is a directory
 ; removes the directory recursively deleting all files
-(provide delete-path)
-(define delete-path storage-not-initialized)
-(define (set-delete-path proc) (set! delete-path proc))
+(fun-maker delete-path)
 
 ; (path -> Either 'file 'directory 'does-not-exist)
 ; Returns a symbol representing if the path is a file, directory, or does not exist
-(provide path-info)
-(define path-info storage-not-initialized)
-(define (set-path-info proc) (set! path-info proc))
-
+(fun-maker path-info)
 
 ; (path -> (listof path))
 ; Returns all files that are at the specified path.
-(provide list-files)
-(define list-files storage-not-initialized)
-(define (set-list-files proc) (set! list-files proc))
+(fun-maker list-files)
 
 ; (path -> (listof path))
 ; Returns all files that are at the specified path recursively adding all files in sub directories
-(provide list-sub-files)
-(define list-sub-files storage-not-initialized)
-(define (set-list-sub-files proc) (set! list-sub-files proc))
+(fun-maker list-sub-files)
 
 ; (path -> (listof path))
 ; Returns all directories that are at the specified path.
-(provide list-dirs)
-(define list-dirs storage-not-initialized)
-(define (set-list-dirs proc) (set! list-dirs proc))
+(fun-maker list-dirs)
 
 ; Loads cloud-storage bindings
 (define (invoke-cloud-storage)
   (define-values/invoke-unit/infer cloud-storage@)
-  (set-retrieve-file retrieve-file)
-  (set-retrieve-file-bytes retrieve-file-bytes)
-  (set-write-file write-file)
-  (set-delete-path delete-path)
-  (set-path-info path-info)
-  (set-list-files list-files)
-  (set-list-sub-files list-sub-files)
-  (set-list-dirs list-dirs))
+  (set! retrieve-file retrieve-file)
+  (set! retrieve-file-bytes retrieve-file-bytes)
+  (set! write-file write-file)
+  (set! delete-path delete-path)
+  (set! path-info path-info)
+  (set! list-files list-files)
+  (set! list-sub-files list-sub-files)
+  (set! list-dirs list-dirs))
 
 ; Loads local storage bindings
 (define (invoke-local-storage)
   (define-values/invoke-unit/infer local-storage@)
-  (set-retrieve-file retrieve-file)
-  (set-retrieve-file-bytes retrieve-file-bytes)
-  (set-write-file write-file)
-  (set-delete-path delete-path)
-  (set-path-info path-info)
-  (set-list-files list-files)
-  (set-list-sub-files list-sub-files)
-  (set-list-dirs list-dirs))
+  (set! retrieve-file retrieve-file)
+  (set! retrieve-file-bytes retrieve-file-bytes)
+  (set! write-file write-file)
+  (set! delete-path delete-path)
+  (set! path-info path-info)
+  (set! list-files list-files)
+  (set! list-sub-files list-sub-files)
+  (set! list-dirs list-dirs))
 
 
 ;; load the appropriate set of functions into the

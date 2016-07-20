23010efbadfb4d417f9a32b920045961d202164f
John Clements <clements@racket-lang.org>
2016-07-20 16:28:51 -0700

    fixes for js

---
+++ html/bin/browse-filecontainer.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// contains js code extracted from browse-file-container.html
+
+function save(json, callback){
+}
+
+function load(callback){
+  callback("{ \"comments\" : {} }");
+}
--- html/bin/filecontainer.js
+++ html/bin/filecontainer.js
@@ -2,11 +2,9 @@
 
 // contains js code extracted from file-container.html
 
-var defaultMode = "@(js-str default-mode)";
-
 function save(json, callback){
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", @(urlgen save-url), true);
+  xhr.open("POST", saveURL, true);
   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
   xhr.send(json);
   xhr.onloadend = callback;
@@ -15,7 +13,7 @@ function save(json, callback){
 function load(callback){
         
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", @(urlgen load-url), true);
+  xhr.open("POST", loadURL, true);
   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
   xhr.send();
   xhr.onreadystatechange = function () {
